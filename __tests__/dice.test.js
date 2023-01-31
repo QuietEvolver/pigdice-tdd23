63e3c48803ead52a7be62326d5a7c5a44401a742
Vera Weikel <vera.weikel@gmail.com>
2023-01-31 13:18:52 -0800

    Add Dice.prototype.PlayRound() definition on dice.js.
    Co-authored by : Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>.

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -16,8 +16,8 @@ describe('Dice', () => {
 
   test('should test prototype RoundScore based on this.number', () =>{
     const dice = new Dice(2, 4, 5, 1);
-    expect(dice.PlayRound()).toEqual(2);
+    expect(dice.PlayRound()).toEqual(7);
     
-  })
+  });
 
 });
