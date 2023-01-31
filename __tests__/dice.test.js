b44b8bfa19b1582a53e28b2a70ce2437b9563dbf
Vera Weikel <vera.weikel@gmail.com>
2023-01-31 11:21:15 -0800

    Add _tests__/dice.test.js to test Dice.proto.Roll.
    Co-authored by : Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>.

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -1,11 +1,20 @@
 import Dice from './../src/dice.js';
 
 describe('Dice', () => {
-    test('should creat a dice object', () => {
-      const dice = new Dice(2, 4, 5, 1);
-      expect(dice.number).toEqual(2);
-      expect(dice.totalScore).toEqual(4);
-      expect(dice.roundScore).toEqual(5);
-      expect(dice.id).toEqual(1);
-    });
+  test('should creat a dice object', () => {
+    const dice = new Dice(2, 4, 5, 1);
+    expect(dice.number).toEqual(2);
+    expect(dice.totalScore).toEqual(4);
+    expect(dice.roundScore).toEqual(5);
+    expect(dice.id).toEqual(1);
+  });
+  
+  test('should test prototype Roll on this.number', () => {
+    const dice = new Dice(2, 4, 5, 1);
+    expect(dice.number).toEqual(2);
+    expect(dice.totalScore).toEqual(4);
+    expect(dice.roundScore).toEqual(5);
+    expect(dice.id).toEqual(1);
+  })
+
 });
