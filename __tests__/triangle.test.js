c496bfcb253f96fc131ab1493b6ebb2732053795
Ashe Urban <ashe@goldentongue.com>
2023-01-30 16:20:31 -0800

    Add final tests.
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -12,4 +12,19 @@ describe('Triangle', () => {
     const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual('not a triangle!');
   });
+
+  test('should correctly determine whether three lengths make a scalene triangle', () => {
+    const scalTriangle = new Triangle(4, 5, 7);
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isoscTriangle = new Triangle(5,5,7)
+    expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+  });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+  const equiTriangle = new Triangle(5,5,5)
+  expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
 });
