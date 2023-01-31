971384de90137fed8576654e1efe348c1918fc4f
Vera Weikel <vera.weikel@gmail.com>
2023-01-31 11:09:54 -0800

    Update dice.js properties; _tests__/dice.test.js to test constructor Dice().
    Co-authored by : Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>.

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -4,8 +4,8 @@ describe('Dice', () => {
     test('should creat a dice object', () => {
       const dice = new Dice(2, 4, 5, 1);
       expect(dice.number).toEqual(2);
-      expect(dice.number).toEqual(4);
-      expect(dice.number).toEqual(5);
-      expect(dice.number).toEqual(1);
-    })
-})
+      expect(dice.totalScore).toEqual(4);
+      expect(dice.roundScore).toEqual(5);
+      expect(dice.id).toEqual(1);
+    });
+});
--- src/dice.js
+++ src/dice.js
@@ -1,6 +1,6 @@
-function Dice(number, totalScore, roundScore, id) {
-  this.number = 0;
-  this.totalScore = 0;
-  this.roundScore = 0;
-  this.id = id; 
-} 
+export default function Dice(number, totalScore, roundScore, id) {
+  this.number = number;
+  this.totalScore = totalScore;
+  this.roundScore = roundScore;
+  this.id = id;
+};
