25d57def2a2176d50230e5a41c2e234db28ae33e
Vera Weikel <vera.weikel@gmail.com>
2023-01-31 11:44:27 -0800

    Add passing _tests__/dice.test.js to test Dice.proto.Roll.
    Co-authored by : Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>.

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -11,10 +11,15 @@ describe('Dice', () => {
   
   test('should test prototype Roll on this.number', () => {
     const dice = new Dice(2, 4, 5, 1);
-    expect(dice.number).toEqual(2);
-    expect(dice.totalScore).toEqual(4);
-    expect(dice.roundScore).toEqual(5);
-    expect(dice.id).toEqual(1);
-  })
+    expect(dice.Roll()).toEqual(4);
+  });
+
+  // test('should test prototype RoundScore based on this.number', () =>{
+    // const dice = new Dice(2, 4, 5, 1);
+    // expect(dice.number).toEqual(2);
+    // expect(dice.totalScore).toEqual(4);
+    // expect(dice.roundScore).toEqual(5);
+    // expect(dice.id).toEqual(1);
+  // })
 
 });
