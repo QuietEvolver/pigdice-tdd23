ee41fbe814a699f5511f3b898e00b00d90b37050
Vera Weikel <vera.weikel@gmail.com>
2023-01-31 11:47:07 -0800

    Add _tests__/dice.test.js to test Dice.proto.PlayRound.
    Co-authored by : Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>.

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -14,12 +14,10 @@ describe('Dice', () => {
     expect(dice.Roll()).toEqual(4);
   });
 
-  // test('should test prototype RoundScore based on this.number', () =>{
-    // const dice = new Dice(2, 4, 5, 1);
-    // expect(dice.number).toEqual(2);
-    // expect(dice.totalScore).toEqual(4);
-    // expect(dice.roundScore).toEqual(5);
-    // expect(dice.id).toEqual(1);
-  // })
+  test('should test prototype RoundScore based on this.number', () =>{
+    const dice = new Dice(2, 4, 5, 1);
+    expect(dice.PlayRound()).toEqual(2);
+    
+  })
 
 });
